9d13b9ecf56e8c2a8afad25687d5f4ecffa4ce39
doganbatuhan <batuuhan.dogan@gmail.com>
2021-10-19 00:06:40 +0300

    18 Ekim Ders Sonu

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ console.log(euroDun)
 
 // arrays
 // camelCasing
-// PascalCasing
+// PascalCasin
 let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
